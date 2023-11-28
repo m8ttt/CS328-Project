ffe3dfbb65145d34f6abc82cb97c079383f624ab
Sharyn <sharyns@nevada.unr.edu>
2023-11-28 00:02:53 -0800

    health bar done, menu done, more assets, added music

---
+++ CS328-Project/Assets/Scripts/HealthBarScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+
+    public Slider slider;
+    public Gradient gradient;
+    public Image filling;
+
+    public void setMaxHealth(int hp){
+        slider.maxValue = hp;
+        slider.value = hp;
+        filling.color = gradient.Evaluate(1f);
+    }
+
+    public void SetHealth(int hp){
+        slider.value = hp;
+        filling.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
---
+++ CS328-Project/Assets/Scripts/PlayerMain.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMain : MonoBehaviour
+{
+
+    public int maxHealth = 50;
+    public int currentHealth;
+    public HealthBarScript HB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        HB.setMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //damage test, delete 
+        if (Input.GetKeyDown(KeyCode.Space)){
+            Damage(10);
+        }
+    }
+
+    //damage test, delete
+    void Damage(int damage){
+        currentHealth -= damage;
+        HB.SetHealth(currentHealth);
+    }
+}
